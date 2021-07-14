39fc53fbc687acc137fdaa55c1fef2a6e83df0cf
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-14 21:52:47 +0000

    functions

--- main.py
+++ main.py
@@ -14,5 +14,36 @@
 # else:
 #   print(f"Your number {number} is negative.")
 
-      
+# Functions
 
+# Definition: inputs provide a single output
+# Programming definition: functions take in inputs (parameters) and return values
+# time consuming and inefficient to not use functions
+# break up code into chunks and reuse it
+# makes code more organized
+
+# Defining a function with no parameters ()
+# def HelloWorld():
+#   print("Hello World")
+
+# Calling a function
+# HelloWorld()
+
+# Function with parameters ()
+# def greeting(name):
+#   print(f"Hello {name}")
+
+# my_name = input("What is your name? ")
+# greeting(my_name)
+
+# Function with no return values = void function
+
+# Return function (returns value)
+# Return statements are always the last thing in the function
+# def addNumber(inputNumber, inputNumber2):
+#   output = int(inputNumber) + int(inputNumber2)
+#   return output
+# num1 = input("Give me a number ")
+# num2 = input("Give me a number ")
+
+# print(addNumber(num1, num2))
