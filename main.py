ea6d408c71a0d02be5043cdf28651a0c677ac10f
hoobab <87278869+good-boi-huan@users.noreply.github.com>
2021-07-14 21:32:58 +0000

    warm up

---
+++ main.py
@@ -0,0 +1,18 @@
+# Warm up
+
+# for i in range(1, 6):
+#   for j in range(0, i):
+#     print(j+1, end="")
+#   print()
+
+# number = int(input("Give me a number."))
+
+# if number > 0:
+#   print(f"Your number {number} is positive.")
+# elif number == 0:
+#   print("Your number is 0.")
+# else:
+#   print(f"Your number {number} is negative.")
+
+      
+
